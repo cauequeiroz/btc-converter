ebf0981cd6a7f01f0739183e6a9f7ceceb9774c2
Caue Queiroz <cauenqueiroz@gmail.com>
2017-11-27 09:06:11 -0200

    Create first test

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -1,5 +1,15 @@
 import { expect } from 'chai';
 
-describe('Main', () => {
+const exec = require('child_process').exec;
+const cli  = './src/main.js';
 
+describe('Main CLI', () => {
+  it('should return Hello World', (done) => {
+    exec(cli, (err, stdout, stderr) => {
+      if (err) throw err;
+
+      expect(stdout.replace('\n', '')).to.be.equal('Hello World');
+      done();
+    });
+  });
 });
