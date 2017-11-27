a6e3d849454c86b5ee918549140f2add40f97809
Caue Queiroz <cauenqueiroz@gmail.com>
2017-11-27 09:29:19 -0200

    Show --version and --help

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -1,14 +1,23 @@
 import { expect } from 'chai';
 
 const exec = require('child_process').exec;
+const pkg  = require('../package.json');
 const cli  = './src/main.js';
 
 describe('Main CLI', () => {
-  it('should return Hello World', (done) => {
-    exec(cli, (err, stdout, stderr) => {
+  it('should return version when `btc-converter --version`', (done) => {
+    exec(`${cli} --version`, (err, stdout, stderr) => {
       if (err) throw err;
 
-      expect(stdout.replace('\n', '')).to.be.equal('Hello World');
+      expect(stdout.replace('\n', '')).to.be.equal(pkg.version);
+      done();
+    });
+  });
+  it('should return description when `btc-converter --help`', (done) => {
+    exec(`${cli} --help`, (err, stdout, stderr) => {
+      if (err) throw err;
+
+      expect(stdout.includes(pkg.description)).to.be.true;
       done();
     });
   });
